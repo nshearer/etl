7d718ad34ec539d7e6e830eb13dfaa112199cf48
Nathan Shearer <nshearer@ewu.edu>
2018-07-11 15:08:30 -0700

    Started schema for recording record data

--- src/netl/tracedb/RecordTrace.py
+++ src/netl/tracedb/RecordTrace.py
@@ -1,6 +1,6 @@
 
 
-from .TraceObject import TraceData
+from .TraceObject import TraceData, TraceAction
 
 class RecordTrace(TraceData):
     '''A record in the ETL'''
@@ -10,11 +10,46 @@ class RecordTrace(TraceData):
     CREATE_STATEMENTS = (
         """\
             create table records (
-              id                int primary key,
-              origin_component  int,
-              large_record      int,
-              data              text)
+              id                  int primary key,
+              origin_component_id int)
+        """,
+        """\
+            create table record_attributes (
+              rec_id              int,
+              attr_name           text,
+              value_id            int)
+        """,
+        """\
+            create table record_data (
+              id                  int primary key,
+              value               text,
+              CONSTRAINT unique_values UNIQUE (value))
         """,
     )
+
+
+class TraceRecord(TraceAction):
+    '''Save record to '''
+
+    def __init__(self, from_port_id, to_port_id):
+        '''
+        :param from_port_id: Unique integer ID of the output port being
+        :param to_port_id: Unique integer ID for the input port
+        '''
+        super(TraceConnection, self).__init__()
+        self.from_port_id = from_port_id
+        self.to_port_id = to_port_id
+
+    def record(self, trace_db):
+        trace_db.execute_update("""
+            insert into connections (output_port_id, input_port_id, state_code)
+            values (?, ?, ?)
+            """, (
+                int(self.from_port_id),
+                int(self.to_port_id),
+                ConnectionTrace.CONN_OPEN))
+
+
+
 
 
