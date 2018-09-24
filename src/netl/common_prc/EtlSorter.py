90e5e17e8dfe4903c8c44c965ab805a6a4718dbb
Nathan Shearer <nshearer@ewu.edu>
2018-09-24 13:32:30 -0700

    Tweaked sorting processor

--- src/netl/common_prc/EtlSorter.py
+++ src/netl/common_prc/EtlSorter.py
@@ -9,16 +9,17 @@ class EtlSorter(EtlComponent):
     input = EtlInput()
     sorted = EtlOutput()
 
-    def __init__(self, sort_key):
+    def __init__(self, sort_key, reverse=False):
         '''
         :param sort_key: Callable to build a string sort key for each record
         '''
         self.__sort_key = sort_key
+        self.__reverse = reverse
         super(EtlSorter, self).__init__()
 
     def run(self):
 
         # TODO: Implement with a record shelf to sort many records
 
-        for rec in sorted(self.input.all(), key = self.__sort_key):
+        for rec in sorted(self.input.all(), key = self.__sort_key, reverse=self.__reverse):
             self.sorted.output(rec)
